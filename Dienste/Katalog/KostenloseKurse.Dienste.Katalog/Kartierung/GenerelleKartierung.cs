4f69aa28edabc53eb0c9d02caa6d3cc1548e123e
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-06-26 11:29:05 +0300

    AutoMapper Library erstellen

---
+++ Dienste/Katalog/KostenloseKurse.Dienste.Katalog/Kartierung/GenerelleKartierung.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using KostenloseKurse.Dienste.Katalog.Düo;
+using KostenloseKurse.Dienste.Katalog.Modelle;
+
+namespace KostenloseKurse.Dienste.Katalog.Kartierung
+{
+    public class GenerelleKartierung:Profile
+    {
+        public GenerelleKartierung()
+        {
+            CreateMap<Kurs, KursDüo>().ReverseMap();
+            CreateMap<Kategorie, KategorieDüo>().ReverseMap();
+            CreateMap<Eigenschaft, EigenschaftDüo>().ReverseMap();
+
+            CreateMap<Kurs, KursErstellenDüo>().ReverseMap();
+            CreateMap<Kurs, KursAktualisierenDüo>().ReverseMap();
+        }
+    }
+}
