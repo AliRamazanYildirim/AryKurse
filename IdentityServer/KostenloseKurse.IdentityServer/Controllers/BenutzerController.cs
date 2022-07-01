6784bc6c0206448c92dd4f0596e2a15fa21232f0
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-01 11:35:38 +0300

    Codierung der Konfigurationsklasse

---
+++ IdentityServer/KostenloseKurse.IdentityServer/Controllers/BenutzerController.cs
@@ -0,0 +1,41 @@
+﻿using KostenloseKurse.IdentityServer.Düo;
+using KostenloseKurse.IdentityServer.Models;
+using KostenloseKurse.Shared.Düo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BenutzerController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BenutzerController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Anmelden(AnmeldenDüo anmeldenDüo)
+        {
+            var benutzer = new ApplicationUser
+            {
+                UserName = anmeldenDüo.BenutzerName,
+                Email = anmeldenDüo.EMail,
+                City = anmeldenDüo.City
+
+            };
+            var resultat = await _userManager.CreateAsync(benutzer, anmeldenDüo.Passwort);
+            if(!resultat.Succeeded)
+            {
+                return BadRequest(Antwort<KeinInhaltDüo>.Fehlschlagen(resultat.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ IdentityServer/KostenloseKurse.IdentityServer/Düo/AnmeldenDüo.cs
@@ -0,0 +1,11 @@
+﻿namespace KostenloseKurse.IdentityServer.Düo
+{
+    public class AnmeldenDüo
+    {
+        public string BenutzerName { get; set; }
+        public string EMail { get; set; }
+        public string Passwort { get; set; }
+        public string City { get; set; }
+
+    }
+}
