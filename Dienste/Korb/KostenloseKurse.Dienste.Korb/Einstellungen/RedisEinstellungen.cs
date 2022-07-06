4be191bb53c4eb4da2baa5accc85ce8e36dd3cba
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-06 15:35:11 +0300

    Revert "RedisService Class(Die Klasse, die für die Verbindung mit Redis verantwortlich ist)"
    This reverts commit b68501edd4ccc0da4cc89e3fed0cdc867985898f.

--- Dienste/Korb/KostenloseKurse.Dienste.Korb/Einstellungen/RedisEinstellungen.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KostenloseKurse.Dienste.Korb.Einstellungen
-{
-    public class RedisEinstellungen
-    {
-        public string Host { get; set; }
-        public int Port { get; set; }
-    }
-}
