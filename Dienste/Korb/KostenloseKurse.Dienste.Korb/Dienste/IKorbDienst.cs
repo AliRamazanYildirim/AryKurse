776990836566873f1e1f50b9d4f9d46a9702bc31
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-06 15:31:41 +0300

    IKorbDienst Interface erstellen

---
+++ Dienste/Korb/KostenloseKurse.Dienste.Korb/Dienste/IKorbDienst.cs
@@ -0,0 +1,13 @@
+﻿using KostenloseKurse.Dienste.Korb.Düo;
+using KostenloseKurse.Shared.Düo;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.Dienste.Korb.Dienste
+{
+    public interface IKorbDienst
+    {
+        Task<Antwort<KorbDüo>> RufKorb(string benutzerID);
+        Task<Antwort<bool>> SpeichernOderAktualisieren(KorbDüo korbDüo);
+        Task<Antwort<bool>> Löschen(string benutzerID);
+    }
+}
