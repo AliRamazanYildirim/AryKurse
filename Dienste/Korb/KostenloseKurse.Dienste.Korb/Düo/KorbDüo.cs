624756d7c18604fe35acfa3a8147d343276ae481
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-06 15:14:08 +0300

    Erstellen von KorbAPI

---
+++ Dienste/Korb/KostenloseKurse.Dienste.Korb/Düo/KorbDüo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KostenloseKurse.Dienste.Korb.Düo
+{
+    public class KorbDüo
+    {
+        public string BenuzterID { get; set; }
+        public string Rabattcode { get; set; }
+        public List<KorbGegenstandDüo> KorbGegenstände { get; set; }
+        public decimal Gesamtpreis { get => KorbGegenstände.Sum(x => x.Preis * x.Menge); }
+    }
+}
---
+++ Dienste/Korb/KostenloseKurse.Dienste.Korb/Düo/KorbGegenstandDüo.cs
@@ -0,0 +1,10 @@
+﻿namespace KostenloseKurse.Dienste.Korb.Düo
+{
+    public class KorbGegenstandDüo
+    {
+        public int Menge { get; set; }
+        public string KursID { get; set; }
+        public string KursName { get; set; }
+        public decimal Preis { get; set; }
+    }
+}
