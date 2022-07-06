6240b0dbaa0460efe9f28e07eaded2bc48f95046
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-06 15:38:33 +0300

    RedisEinstellungen(Services) erstellen

---
+++ Dienste/Korb/KostenloseKurse.Dienste.Korb/Einstellungen/RedisEinstellungen.cs
@@ -0,0 +1,8 @@
+﻿namespace KostenloseKurse.Dienste.Korb.Einstellungen
+{
+    public class RedisEinstellungen
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
