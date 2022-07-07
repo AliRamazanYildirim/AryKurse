1d1e666acb0c79290b75f690319838ec6a937bd0
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-07 12:36:01 +0300

    Rabatt-Modell definieren

---
+++ Dienste/Rabatt/KostenloseKurse.Dienste.Rabatt/Modell/Rabatt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KostenloseKurse.Dienste.Rabatt.Modell
+{
+    [Dapper.Contrib.Extensions.Table("rabatt")]
+    public class Rabatt
+    {
+        public int ID { get; set; }
+        public string BenutzerID { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime ErstellungsDatum  { get; set; }
+    }
+}
