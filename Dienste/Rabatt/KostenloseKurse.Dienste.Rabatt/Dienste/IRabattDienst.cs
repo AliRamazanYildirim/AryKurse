c535a74e09500e0b6edd8a49b02ec996b6af35a9
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-07 13:03:05 +0300

    IRabattDienst Klasse in Modell Ordner erstellen

---
+++ Dienste/Rabatt/KostenloseKurse.Dienste.Rabatt/Dienste/IRabattDienst.cs
@@ -0,0 +1,19 @@
+﻿using KostenloseKurse.Shared.Düo;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.Dienste.Rabatt.Dienste
+{
+    public interface IRabattDienst
+    {
+        Task<Antwort<List<Modell.Rabatt>>>NehmenAlleDaten();
+        Task<Antwort<Modell.Rabatt>> RufenNachIDAuf(int ID);
+        Task<Antwort<KeinInhaltDüo>> Speichern(Modell.Rabatt rabatt);
+        Task<Antwort<KeinInhaltDüo>> Aktualisieren(Modell.Rabatt rabatt);
+        Task<Antwort<KeinInhaltDüo>> Löschen(int ID);
+        Task<Antwort<Modell.Rabatt>> RufenNachCodeUndBenutzerID(string code, string benutzerID);
+
+
+
+    }
+}
