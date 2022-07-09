28b883871146aed3ea70ab73e89e9d7b661de4d6
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-09 17:49:30 +0300

    Hinzufügen der Automapper-Bibliothek

---
+++ Dienste/Bestellung/KostenloseKurse.Dienste.Bestellung.Anwendung/Kartierung/BrauchKartierung.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using KostenloseKurse.Dienste.Bestellung.Anwendung.Düo;
+using KostenloseKurse.Dienste.Bestellung.Domain.BestellungsAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.Dienste.Bestellung.Anwendung.Kartierung
+{
+    internal class BrauchKartierung:Profile
+    {
+        public BrauchKartierung()
+        {
+            CreateMap<Domain.BestellungsAggregate.Bestellung, BestellungsDüo>().ReverseMap();
+            CreateMap<BestellungsArtikel, BestellungsArtikelDüo>().ReverseMap();
+            CreateMap<Adresse, AdresseDüo>().ReverseMap();
+        }
+    }
+}
---
+++ Dienste/Bestellung/KostenloseKurse.Dienste.Bestellung.Anwendung/Kartierung/ObjektMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.Dienste.Bestellung.Anwendung.Kartierung
+{
+    public static class ObjektMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var konfig = new MapperConfiguration(konfig =>
+            {
+                konfig.AddProfile<BrauchKartierung>();
+            });
+
+            return konfig.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
