dbdbed2004f635efe8084d01c4aeb61a45efca43
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-10 14:16:22 +0300

    Erstellen von Fake-Zahlung-API(Microservice)

---
+++ Dienste/FakeZahlung/KostenloseKurse.Dienste.FakeZahlung/Controllers/FakeZahlungController.cs
@@ -0,0 +1,19 @@
+﻿using KostenloseKurse.Shared.ControllerBasis;
+using KostenloseKurse.Shared.Düo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KostenloseKurse.Dienste.FakeZahlung.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakeZahlungController : BrauchBasisController
+    {
+        [HttpPost]
+        public IActionResult ZahlungErhalten()
+        {
+            return ErstellenAktionResultatBeispiel(Antwort<KeinInhaltDüo>.Erfolg(200));
+
+        }
+    }
+}
