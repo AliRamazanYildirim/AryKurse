8d92ac82042e5ebb631af1e0350b98579bdf7df6
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-10 16:40:45 +0300

    konfigurationsentwicklung.json(Katalog-Microservice) und andere Microservices

--- Dienste/Katalog/KostenloseKurse.Dienste.Katalog/Program.cs
+++ Dienste/Katalog/KostenloseKurse.Dienste.Katalog/Program.cs
@@ -1,5 +1,8 @@
+using KostenloseKurse.Dienste.Katalog.Dienste;
+using KostenloseKurse.Dienste.Katalog.Düo;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,22 @@ namespace KostenloseKurse.Dienste.Katalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dienstAnbieter = scope.ServiceProvider;
+
+                var kategorieDienst = dienstAnbieter.GetRequiredService<IKategorieDienst>();
+
+                if (!kategorieDienst.RufAlleDatenAsync().Result.Daten.Any())
+                {
+                    kategorieDienst.ErstellenAsync(new KategorieDüo { Name = "Asp.net Core Kurs" }).Wait();
+                    kategorieDienst.ErstellenAsync(new KategorieDüo { Name = "Asp.net Core API Kurs" }).Wait();
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
