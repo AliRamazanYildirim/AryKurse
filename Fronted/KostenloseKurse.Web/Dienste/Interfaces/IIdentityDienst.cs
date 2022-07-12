368f0bb24d68f319add91ad4cec3449970dc0980
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-07-12 15:35:00 +0300

    Bootstrap 5, IIdentitiyDienst und IdentityModel library hinzufügen

---
+++ Fronted/KostenloseKurse.Web/Dienste/Interfaces/IIdentityDienst.cs
@@ -0,0 +1,16 @@
+﻿using IdentityModel.Client;
+using KostenloseKurse.Shared.Düo;
+using KostenloseKurse.Web.Models;
+using System.Threading.Tasks;
+
+namespace KostenloseKurse.Web.Dienste.Interfaces
+{
+    public interface IIdentityDienst
+    {
+        Task<Antwort<bool>> Einloggen(EingabeAnmelden eingabeAnmelden);
+
+        Task<TokenResponse> RufenAccessTokenNachAktualisierungsTokenAuf();
+
+        Task WiderrufenAktualisierungsToken();
+    }
+}
---
+++ Fronted/KostenloseKurse.Web/Models/EingabeAnmelden.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KostenloseKurse.Web.Models
+{
+    public class EingabeAnmelden
+    {
+        [Required]
+        [Display(Name = "Ihre Emailadresse")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Ihr Passwort")]
+        public string Passwort{ get; set; }
+
+        [Display(Name = "Erinnere dich an mich")]
+        public bool ErinnereDich { get; set; }
+    }
+}
